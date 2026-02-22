4478064afd8e286e6568ae93134c5a452536dd2f
Yousef Dawood <yousefdawood31@gmail.com>
2026-02-22 22:25:46 +0200

    add functions to calculate the sum of the first N natural numbers with time complexity analysis

---
+++ sum-n-natural-number/sum-n-natural-number.ts
@@ -0,0 +1,89 @@
+const firstNNaturalNumbers = function (number: number) {
+  return (number * (number + 1)) / 2;
+};
+
+/*
+  TIME COMPLEXITIES
+    Worst Case => O(1)
+    Average Case => O(1)
+    Best Case => O(1)
+  
+  SPACE COMPLEXITY ===> O(1)
+  
+  Asymptotic Notations
+    Omega =>  Ω(1)
+    Theta => Θ(1)
+    Big O => O(1)
+*/
+
+const firstNNaturalNumbersIterative = function (number: number) {
+  let result = 0;
+
+  // prettier-ignore
+  for (let i = 1; i <= number; ++i)
+    result += i;
+
+  return result;
+};
+
+/*
+  TIME COMPLEXITIES
+    Worst Case => O(n)
+    Average Case => O(n)
+    Best Case => O(1)
+  
+  SPACE COMPLEXITY ===> O(1)
+  
+  Asymptotic Notations
+    Omega =>  Ω(1)
+    Theta => Θ(n) ==> for Worst Case
+    Big O => O(n)
+*/
+
+const firstNNaturalNumbersNestedIterative = function (number: number) {
+  let result = 0;
+
+  // prettier-ignore
+  for (let i = 1; i <= number; ++i)
+    for (let j = 1; j <= number; ++j)
+     ++result;
+
+  // This Will Run For The Same Formula As The First Algorithm in This file
+  return result;
+};
+
+/*
+  TIME COMPLEXITIES
+    Worst Case => O(n^2)
+    Average Case => O(n^2)
+    Best Case => O(1)
+  
+  SPACE COMPLEXITY ===> O(1)
+  
+  Asymptotic Notations
+    Omega =>  Ω(1)
+    Theta => Θ(n^2) ==> for Worst Case
+    Big O => O(n^2)
+*/
+
+const firstNNaturalNumbersNestedRecursive = function (number: number): number {
+  // prettier-ignore
+  if (number === 0)
+    return 0;
+
+  return 1 + firstNNaturalNumbersNestedRecursive(number - 1);
+};
+
+/*
+  TIME COMPLEXITIES
+    Worst Case => O(n)
+    Average Case => O(n)
+    Best Case => O(1)
+  
+  SPACE COMPLEXITY ===> O(n)
+  
+  Asymptotic Notations
+    Omega =>  Ω(1)
+    Theta => Θ(n) ==> for Worst Case
+    Big O => O(n)
+*/
