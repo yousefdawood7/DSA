b83320bb1b7ed487ca8223cbd242738d1d477e57
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-11 02:19:52 +0300

    refactor twoSum function to throw an error for no valid solution and improve clarity; add twoSum2 implementation with optimized time complexity

--- array/two_sum/two_sum.ts
+++ array/two_sum/two_sum.ts
@@ -2,12 +2,28 @@ export const twoSum = function (nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length; ++i)
     for (let j = i + 1; j < nums.length; ++j)
       if (nums[i] + nums[j] === target) return [i, j];
-  return [];
+  throw new Error("No valid solution found");
 };
-
-// Problem: https://leetcode.com/problems/two-sum
 
 /*
   TIME COMPLEXITIES ==> O(n^2)
   SPACE COMPLEXITY ==> O(1)
 */
+
+export const twoSum2 = function (nums: number[], target: number): number[] {
+  const indicies: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; ++i) {
+    // prettier-ignore
+    if (indicies.has(target - nums[i]))
+        return [indicies.get(target - nums[i])!, i];
+    indicies.set(nums[i], i);
+  }
+  throw new Error("No valid solution found");
+};
+
+/*
+  TIME COMPLEXITIES ==> O(n)
+  SPACE COMPLEXITY ==> O(n)
+*/
+
+// Problem: https://leetcode.com/problems/two-sum
