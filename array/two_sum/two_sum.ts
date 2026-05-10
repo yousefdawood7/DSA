183d6e0f84451c579554e42e9a4387600c927015
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-10 22:50:22 +0300

    refactor twoSum function to remove undefined return type and improve clarity

--- array/two_sum/two_sum.ts
+++ array/two_sum/two_sum.ts
@@ -1,11 +1,11 @@
-export const twoSum = function (
-  nums: number[],
-  target: number,
-): number[] | undefined {
+export const twoSum = function (nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length; ++i)
     for (let j = i + 1; j < nums.length; ++j)
       if (nums[i] + nums[j] === target) return [i, j];
+  return [];
 };
+
+// Problem: https://leetcode.com/problems/two-sum
 
 /*
   TIME COMPLEXITIES ==> O(n^2)
