cf9010652c9bc3c0fbdfbf65fd38dc4cbfd39b09
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-16 02:33:54 +0300

    implement longestCommonPrefix function to find the longest common prefix among an array of strings

---
+++ array/longest_common_prefix/longest_common_prefix.ts
@@ -0,0 +1,42 @@
+export function longestCommonPrefix(strs: string[]): string {
+  // prettier-ignore
+  if (!strs.length)
+    return "";
+
+  // prettier-ignore
+  if(strs.length === 1)
+    return strs[0]
+
+  const firstWord = strs[0];
+  let prefixLetters = "";
+  let currentSlice = "";
+
+  for (let i = 0; i < firstWord.length; ++i) {
+    let isCommonPrefix = true; // we using this flag to ensure the rest of the words has the same prefix
+    currentSlice += firstWord[i];
+
+    for (let j = 0; j < strs.length; ++j) {
+      let slicedString = "";
+      for (
+        let k = 0;
+        k < strs[j].length && k <= i;
+        ++k // we want the slice of string's slice to be the as the currentSlice
+      )
+        slicedString += strs[j][k];
+
+      // prettier-ignore
+      if (currentSlice !== slicedString)
+        isCommonPrefix = false;
+    }
+    /**
+      if it's not a common prefix we return the last common prefix we had 
+      (because at that point we will have the current common prefix)
+    */
+    // prettier-ignore
+    if (!isCommonPrefix) 
+        return prefixLetters
+    prefixLetters += firstWord[i];
+  }
+
+  return prefixLetters;
+}
